9811076e0236318a3dcf6741b1c01defbc685aa1
Liz Couture <lizcouture@geisel-software.com>
2017-07-11 11:51:37 -0400

    Added console support

--- www/js/alyssa-edit.js
+++ www/js/alyssa-edit.js
@@ -2,12 +2,18 @@ window.onload = function(){
     var data = loadDataFromJson("sampleData.json");
     var sched = document.getElementById('schedule');
     var innerHtml = "";
-
+    console.log("i can log something.  Anything.");
 
     //TODO: Figure out a way to do this that doesn't involve complete writing
     // all the HTML from Javascript lol
     for (var i = 0; i < data.length; i++){
-        $("#schedule").load("./schedule_block.html");
+        var scheduleBlock = $("<tr>", {id: '#schedBlock' + i, class: 'block shadow'});
+        scheduleBlock.load("./schedule_block.html");
+        console.log("test");
+        var tbody = $("#schedule").find("tbody");
+        console.log("I found a tbody!" + tbody);
+
+        tbody.append(scheduleBlock);
         /*    var liString = "<li class='scheduleTesting_item'>";
               liString += "<div class='left_half'><p class='time'>" + data[i]['time'];
               liString += "</p></div>";
